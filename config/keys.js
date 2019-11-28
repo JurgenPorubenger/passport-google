0762c689fc68d4a6ab65e2ddb53e260cc06fe8cd
jurgen <jurgenporubenger@gmail.com>
2019-11-29 00:09:47 +0200

    passport_Google

---
+++ config/keys.js
@@ -0,0 +1,14 @@
+// add this file to .gitignore
+
+module.exports = {
+        google: {
+            clientID: '741869394675-4irvjmm9rvqtpn8mjgteam00i5319nh8.apps.googleusercontent.com',
+            clientSecret: 'HOzulep5hwMYd00IRc33gp5S'
+        },
+        mongodb: {
+            dbURI: 'mongodb://127.0.0.1:27017/mydbs'
+        },
+        session: {
+            cookieKey: 'thenetninjaisawesomeiguess'
+        }
+};
